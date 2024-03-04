0359178e16b8c6cbba23aa8402ad1bc1478519b4
Auniella Tanoh <0110a2002@gmail.com>
2024-03-04 19:03:42 +0000

    Ajout d'une sonnerie à la fin du minuteur

--- script.js
+++ script.js
@@ -1,59 +1,12 @@
-/*let hoursInput, minutesInput, secondsInput, timer;
-
-window.onload = function () {
-  hoursInput = document.getElementById("hours");
-  minutesInput = document.getElementById("minutes");
-  secondsInput = document.getElementById("seconds");
-  btn_start = document.getElementById("btn_start");
-  btn_stop = document.getElementById("btn_stop");
-
-  btn_start.addEventListener("click", startTimer);
-  btn_stop.addEventListener("click", stopTimer);
-};
-
-function startTimer() {
-  let hours = parseInt(hoursInput.value) || 0;
-  let minutes = parseInt(minutesInput.value) || 0;
-  let seconds = parseInt(secondsInput.value) || 0;
-
-  if (hours === 0 && minutes === 0 && seconds === 0) {
-    alert("Veuillez entrer une durée supérieure à zéro.");
-    return;
-  }
-
-  let totalSeconds = hours * 3600 + minutes * 60 + seconds;
-
-  timer = setInterval(function () {
-    if (totalSeconds <= 0) {
-      clearInterval(timer);
-      alert("Minuteur terminé !");
-      return;
-    }
-
-    let hoursRemaining = Math.floor(totalSeconds / 3600);
-    let minutesRemaining = Math.floor((totalSeconds % 3600) / 60);
-    let secondsRemaining = totalSeconds % 60;
-
-    hoursInput.value = String(hoursRemaining).padStart(2, "0");
-    minutesInput.value = String(minutesRemaining).padStart(2, "0");
-    secondsInput.value = String(secondsRemaining).padStart(2, "0");
-
-    totalSeconds--;
-  }, 1000);
-}
-
-function stopTimer() {
-  clearInterval(timer);
-}
-*/
-
 let hoursInput,
   minutesInput,
   secondsInput,
   timer,
   btn_start,
   btn_stop,
-  btn_reset;
+  btn_reset,
+  container;
+const audio = new Audio("sound/Radar-iPhone.mp3");
 
 window.onload = function () {
   hoursInput = document.getElementById("hours");
@@ -65,6 +18,7 @@ window.onload = function () {
   btn_start = document.getElementById("btn_start");
   btn_stop = document.getElementById("btn_stop");
   btn_reset = document.getElementById("btn_reset");
+  container = document.getElementById("container");
 
   btn_start.addEventListener("click", start);
   btn_stop.addEventListener("click", stop);
@@ -91,6 +45,9 @@ function start() {
       secondsInput.value = "00";
 
       btn_start.disabled = false;
+      audio.loop = true;
+      audio.play();
+      container.appendChild("audio");
       alert("Terminé");
       return;
     }
@@ -113,6 +70,8 @@ function stop() {
   clearInterval(timer);
 
   btn_start.disabled = false;
+  audio.pause();
+  audio.currentTime = 0;
 }
 
 function reset() {
